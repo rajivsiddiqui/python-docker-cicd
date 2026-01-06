1dd26b40befcf2033c949ebc1a1f28368cd54223
Rajiv Siddiqui <38841463+rajivsiddiqui@users.noreply.github.com>
2026-01-06 20:57:04 +0600

    Update cicd.py

--- cicd.py
+++ cicd.py
@@ -5,7 +5,7 @@ import requests
 
 IMAGE = "devopssteps/node-demo:latest"
 CONTAINER = "node-demo"
-SLACK_WEBHOOK = "https://hooks.slack.com/services/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"
+SLACK_WEBHOOK = "my-slack-url"
 
 def run_cmd(cmd):
     subprocess.check_call(cmd, shell=True)
@@ -32,7 +32,7 @@ def email_notify(msg):
 
     with smtplib.SMTP("smtp.gmail.com", 587) as server:
         server.starttls()
-        server.login(sender, "gvndpezcjsquysqb")
+        server.login(sender, "aaaa")
         server.send_message(email)
 
 if __name__ == "__main__":
